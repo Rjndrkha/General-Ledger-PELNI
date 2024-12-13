d6a16e70731fbe7b144336feaf1f8ab89e8b5167
aldysa <aldysayusuf@gmail.com>
2024-12-13 10:54:14 +0700

    Cleaning Up Code

--- frontend/src/page/generalLedger/table.tsx
+++ frontend/src/page/generalLedger/table.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
-import { greenDark } from "@ant-design/colors";
-import { SearchOutlined } from "@ant-design/icons";
-import type { GetRef, TableColumnsType, TableColumnType } from "antd";
-import { Button, Input, Space, Table, message, Spin } from "antd";
-import type { FilterDropdownProps } from "antd/es/table/interface";
-import Highlighter from "react-highlight-words";
+import React, { useEffect, useState } from "react";
+import type { TableColumnsType } from "antd";
+import { Button, Table, message} from "antd";
 import Cookies from "js-cookie";
 import EbsClient from "../../service/ebs/OracleClient";
 import { ITableGeneralLedger } from "../../interface/ITableGeneralLedger";
 import ButtonDefault from "../../component/button/button";
 import { downloadExcelFile } from "../../utils/excelUtils";
 
-type InputRef = GetRef<typeof Input>;
-type DataIndex = keyof ITableGeneralLedger;
-
 const TableGeneralLedger: React.FC = () => {
   const [dataInput, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
-  const searchInput = useRef<InputRef>(null);
   const token = Cookies.get("token");
 
   useEffect(() => {
@@ -46,16 +36,6 @@ const TableGeneralLedger: React.FC = () => {
     }
 
     setLoading(false);
-  };
-
-  const handleSearch = (
-    selectedKeys: string[],
-    confirm: FilterDropdownProps["confirm"],
-    dataIndex: DataIndex
-  ) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
   };
 
   const handleButtonStatus = (status: string, job_id: Number) => {
@@ -95,106 +75,6 @@ const TableGeneralLedger: React.FC = () => {
       message.error("Data tidak tersedia");
     }
   };
-
-  const handleReset = (
-    clearFilters: () => void,
-    confirm: FilterDropdownProps["confirm"]
-  ) => {
-    clearFilters();
-    setSearchText("");
-    confirm();
-  };
-
-  const getColumnSearchProps = (
-    dataIndex: DataIndex
-  ): TableColumnType<ITableGeneralLedger> => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-      close,
-    }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() =>
-            handleSearch(selectedKeys as string[], confirm, dataIndex)
-          }
-          style={{ marginBottom: 8, display: "block" }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() =>
-              handleSearch(selectedKeys as string[], confirm, dataIndex)
-            }
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters, confirm)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false });
-              setSearchText((selectedKeys as string[])[0]);
-              setSearchedColumn(dataIndex);
-            }}
-          >
-            Filter
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.focus(), 100);
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ""}
-        />
-      ) : (
-        text
-      ),
-  });
 
   const formatDate = (dateString: string): string => {
     if (!dateString) return "-";
@@ -254,7 +134,6 @@ const TableGeneralLedger: React.FC = () => {
       dataIndex: "status",
       key: "status",
       render: (text, record) => handleButtonStatus(text, Number(record.job_id)),
-      ...getColumnSearchProps("status"),
     },
   ];
 
