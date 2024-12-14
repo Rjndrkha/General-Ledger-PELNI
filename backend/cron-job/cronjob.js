51eff9592513013e0fb4e5a75743757de01af1f4
Rajendra Rakha <rajendra.rakha29@gmail.com>
2024-12-14 09:52:29 +0700

    Feat : Add Cluster For Optimize Backend

--- backend/cron-job/cronjob.js
+++ backend/cron-job/cronjob.js
@@ -1,15 +1,19 @@
 const cleanOldFiles = require("../controllers/generalLedgerController/jobs/cleanGeneralLedger");
 const cron = require("node-cron");
+const cluster = require("cluster");
 
 function initializeCronJobs() {
   // Jadwalkan pembersihan setiap hari pada jam 2 pagi
-  cron.schedule("0 2 * * *", async () => {
-    console.log("Memulai pembersihan file lama...");
-    await cleanOldFiles();
-    console.log("Pembersihan selesai.");
-  });
 
-  console.log("Cron jobs telah dijadwalkan.");
+  if (cluster.isWorker && cluster.worker.id === 1) {
+    cron.schedule("0 2 * * *", async () => {
+      console.log("Memulai pembersihan file lama...");
+      await cleanOldFiles();
+      console.log("Pembersihan selesai.");
+    });
+
+    console.log("Cron jobs telah dijadwalkan.");
+  }
 }
 
 module.exports = initializeCronJobs;
