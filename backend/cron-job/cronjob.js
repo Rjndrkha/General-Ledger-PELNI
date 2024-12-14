b6983dd16e21025a559bd64e669eed1bc6898b3f
Rajendra Rakha <rajendra.rakha29@gmail.com>
2024-12-14 10:46:40 +0700

    FIX : Revert Error Code

--- backend/cron-job/cronjob.js
+++ backend/cron-job/cronjob.js
@@ -1,19 +1,16 @@
 const cleanOldFiles = require("../controllers/generalLedgerController/jobs/cleanGeneralLedger");
 const cron = require("node-cron");
-const cluster = require("cluster");
 
 function initializeCronJobs() {
   // Jadwalkan pembersihan setiap hari pada jam 2 pagi
 
-  if (cluster.isWorker && cluster.worker.id === 1) {
-    cron.schedule("0 2 * * *", async () => {
-      console.log("Memulai pembersihan file lama...");
-      await cleanOldFiles();
-      console.log("Pembersihan selesai.");
-    });
+  cron.schedule("0 2 * * *", async () => {
+    console.log("Memulai pembersihan file lama...");
+    await cleanOldFiles();
+    console.log("Pembersihan selesai.");
+  });
 
-    console.log("Cron jobs telah dijadwalkan.");
-  }
+  console.log("Cron jobs telah dijadwalkan.");
 }
 
 module.exports = initializeCronJobs;
